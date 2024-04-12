6682dc6515e53a05cf8d77a8c0d87ea315a79424
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-04-12 16:35:18 +0300

    solution

--- task.sql
+++ task.sql
@@ -38,3 +38,18 @@ CREATE TABLE ProductInventory (
     PRIMARY KEY (ID)
 );
 --rollback DROP TABLE ProductInventory;
+
+--changeset sqlyaas:5 labels:0.0.2
+CREATE TABLE Users (
+    ID INT AUTO_INCREMENT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+--rollback DROP TABLE ProductInventory;
+
+--changeset sqlyaas:6 labels:0.0.3
+CREATE INDEX idx_email ON Users (Email); 
+
+--rollback DROP TABLE ProductInventory;
